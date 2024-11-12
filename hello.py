4df39d6521e14e52a5787e5da855fb8f6827e00d
paideia <99157069+paideia1200bce@users.noreply.github.com>
2024-11-11 22:16:48 -0800

    Add CloudFormation template to deploy Lambda

--- hello.py
+++ hello.py
@@ -1,9 +1,8 @@
-# TestFile.Line, "content-layout: bearer":
-  content-length: -1
-  default: "Lambda"
-
-export default
-  # Generated from lambda.SetDemo.Plan.and.Italian.Lambda
-  encoding = "Lambda
-  "id":
-  "name": "Hello World", "exp": "0", "content": ["Hello World"]
+# CloudUp string
+def lambda(context, context_lambda, context_params, memory_id):
+    context = context
+    context_lambda = context_lambda
+    context_params = context_params
+    memory_id = memory_id
+    lambda = 'Hello World!'
+    params = 'secure this lambda application!'
