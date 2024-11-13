be46ce7035a1f86a033d5eb9f2a6b9c1c499777c
paideia <99157069+paideia1200bce@users.noreply.github.com>
2024-11-12 18:14:49 -0800

    Retrieve contents of hello.py file

--- hello.py
+++
@@ -1,8 +0,0 @@
-# CloudUp string
-def lambda(context, context_lambda, context_params, memory_id):
-    context = context
-    context_lambda = context_lambda
-    context_params = context_params
-    memory_id = memory_id
-    lambda = 'Hello World!'
-    params = 'secure this lambda application!'
